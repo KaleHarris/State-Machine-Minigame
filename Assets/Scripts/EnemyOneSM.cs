afe86460cc2f66a76081689a48a8305b01ba969e
Gbr1ck <gbrick12345@gmail.com>
2020-09-09 13:59:00 -0600

    Added VR and Enemy

---
+++ Assets/Scripts/EnemyOneSM.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyOneSM : MonoBehaviour
+{
+    //define agent for NavMeash
+    public NavMeshAgent agentOne;
+    //For finding player
+    public Transform player;
+    //create Layermasks
+    public LayerMask platformLayer, playerLayer;
+
+    //Scan
+    public Vector3 Scanning;
+    bool SetScan;
+    public float scanningRange;
+
+    //Attack
+    public float attackTimer;
+    bool Attacked;
+
+    //States
+    public float inSight, attackRange;
+    public bool playerInRange, playerInAttackRange;
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player").transform;
+        agentOne = GetComponent<NavMeshAgent>();
+    }
+    
+    // Update is called once per frame
+    private void Update()
+    {
+        //check to see if player is in sight and in attack range
+        playerInRange = Physics.CheckSphere(transform.position, inSight, playerLayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, playerLayer);
+
+        //when the player is not in attack range or the chase range run the Scan function
+        if (!playerInRange && !playerInAttackRange) Scan();
+        //when the player is in chase range but not in attack range run the Chase function
+        if (playerInRange && !playerInAttackRange) Chase();
+        //when the player is in range and close enough to attack run the Attack function
+        if (playerInRange && playerInAttackRange) Attack();
+
+    }
+    //Function for finding the player
+    private void Scan()
+    {
+        Debug.Log("I Am Scanning");
+
+        if (!SetScan) ScanningArea();
+
+        if (SetScan)
+            agentOne.SetDestination(Scanning);
+
+        Vector3 distanceToDestination = transform.position - Scanning;
+
+        if (distanceToDestination.magnitude < 1f)
+            SetScan = false;
+    }
+    
+    private void ScanningArea()
+    {
+        float RandomZ = Random.Range(-scanningRange, scanningRange);
+        float RandomX = Random.Range(-scanningRange, scanningRange);
+
+        Scanning = new Vector3(transform.position.x + RandomX, transform.position.y, transform.position.z + RandomZ);
+
+        if (Physics.Raycast(Scanning, -transform.up, 2f, platformLayer))
+            SetScan = true;
+    }
+    //Function for Chasing the player
+    private void Chase()
+    {
+        Debug.Log("I Am Chasing");
+        agentOne.SetDestination(player.position);
+    }
+    //Function for attacking the player
+    private void Attack()
+    {
+        Debug.Log("I Am Attacking");
+        agentOne.SetDestination(player.position);
+
+        transform.LookAt(player);
+
+        if (!Attacked)
+        {
+            // NEED TO ADD SOME FORM OF ATTACK HERE
+
+            Attacked = true;
+            Invoke(nameof(ResetAttack), attackTimer);
+        }
+    }
+
+    private void ResetAttack()
+    {
+        Attacked = false;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public int speed = 5;
+
+    private Rigidbody playerRB;
+
+    public float JumpForce = 5;
+
+    public bool isOnGround = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+        transform.position = new Vector3(0, 3, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontalInput, 0, verticalInput);
+
+        transform.Translate(direction * speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRB.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
+    }
+
+}
