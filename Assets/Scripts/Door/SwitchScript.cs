a949b93ee810d3ff5dcd1702e7547b12963bb099
Leafygreens97 <kshx5@yahoo.com>
2020-10-15 14:23:20 -0600

    Added door buttons
    Buttons toggle the doors to close or open, which do so by sliding in or out of the ground. Player activates the buttons by moving hands into them.

---
+++ Assets/Scripts/Door/SwitchScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchScript : MonoBehaviour
+{
+    public GameObject targetDoor;
+    bool isOpen = false;
+    bool shouldMove = false;
+    private float openPosition = -20f, closePosition = 16.46667f;
+    public float  doorSpeed = 20f;
+
+    
+    void Update()
+    {
+        if (shouldMove == true){
+            MoveDoor();
+        }
+      
+    }
+
+    void OnTriggerEnter(Collider Other){
+        if(Other.gameObject.CompareTag("Hands")){
+            shouldMove = true;
+        }
+    }
+
+    void MoveDoor(){
+        if (isOpen == false && targetDoor.transform.position.y > openPosition){
+            Vector3 newPos = targetDoor.transform.position;
+            newPos.y -= doorSpeed * Time.deltaTime;
+            targetDoor.transform.position = newPos;
+            if(targetDoor.transform.position.y <= openPosition){
+                isOpen = true;
+            shouldMove = false;
+            }  
+        }
+        else if (isOpen == true && targetDoor.transform.position.y < closePosition){
+            Vector3 newPos = targetDoor.transform.position;
+            newPos.y += doorSpeed * Time.deltaTime;
+            targetDoor.transform.position = newPos;
+            if (targetDoor.transform.position.y >= closePosition){
+                isOpen = false;
+            shouldMove = false;
+            }
+        }
+    }
+}
