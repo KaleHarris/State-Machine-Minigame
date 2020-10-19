74db9f1e0a5c49df4d2b0c1c69f54abd810550f4
Gbr1ck <gbrick12345@gmail.com>
2020-10-18 21:20:45 -0600

    Added health bar and slime fuel bar

---
+++ Assets/HealthTracking.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthTracking : MonoBehaviour
+{
+    public int startHealth = 1000;
+    public int currentHealth;
+    public HealthBar healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = startHealth;
+        healthBar.SetMaxHealth(startHealth);
+    }
+
+    void takeHealth(int health)
+    {
+        currentHealth -= health;
+
+        healthBar.SetHealth(currentHealth);
+    }
+
+    public void OnCollisionStay(Collision other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("GOT HERE");
+            takeHealth(1);
+        }
+    }
+}
